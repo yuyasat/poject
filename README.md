f41d34aaf0cfac7fb0802aea5dfe6c018c3c42ae
yuyasat <takiyour@gmail.com>
2017-02-18 18:18:29 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+* Ruby version
+ - 2.4.0
+
+* Database creation
+ - `bin/rails db:create`
+ - `bin/rails db:migrate`
+
+* Database initialization
+ - `bin/rails db:seed`
+
+* How to run the test suite
+ - `RAILS_ENV=test bin/rspec spec`
+
+* About frontend
+ - `cd frontend`
+ - `yarn install`
+ - Use command `yarn run watch` when developing.
+
+* Deployment instructions
---
+++ frontend/config/webpack.production.js
@@ -0,0 +1,32 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    application: './src/javascripts/application.js',
+  },
+  output: {
+    path: '../app/assets/javascripts',
+    filename: '[name].js'
+  },
+  plugins: [
+    new webpack.ProvidePlugin({ $: 'jquery', jQuery: 'jquery' })
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel?presets[]=es2015,presets[]=stage-2'
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass']
+      },
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
+      {
+        test: /\.(jpe|jpg|woff|woff2|eot|ttf|svg)(\?.*$|$)/,
+        loader: 'url-loader?mimetype=application/font-woff'
+      }
+    ]
+  }
+}
