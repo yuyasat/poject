44b2e2d426e2b81172e0175f2b4e988151332909
yuyasat <takiyour@gmail.com>
2017-02-27 00:27:07 +0900

    change Grid to stateless component

---
+++ frontend/src/javascripts/poject/components/Grid/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './Styles.css'
+
+export default function Grid(props) {
+  const colorName = props.gridState.color === 0 ? '' : `Grid${props.gridState.color}`
+
+  return (
+    <div className={`Grid ${colorName}`} />
+  )
+}
