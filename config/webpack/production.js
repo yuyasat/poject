7764f4610f91ffbab37ecd55255ce3207b8ed7d0
yuyasat <takiyour@gmail.com>
2017-11-23 09:54:32 +0900

    update webpack configs

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,3 +1,33 @@
 const environment = require('./environment')
+const ManifestPlugin = require('webpack-manifest-plugin')
 
-module.exports = environment.toWebpackConfig()
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = Object.assign({}, environment.toWebpackConfig(), {
+  plugins: [
+    new webpack.ProvidePlugin({ $: 'jquery', jQuery: 'jquery' }),
+    new ManifestPlugin({ publicPath: '/packs/', writeToFileEmit: true })
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react', 'stage-1', 'stage-2']
+        }
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass']
+      },
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
+      {
+        test: /\.(jpe|jpg|woff|woff2|eot|ttf|svg)(\?.*$|$)/,
+        loader: 'url-loader?mimetype=application/font-woff'
+      }
+    ]
+  }
+})
